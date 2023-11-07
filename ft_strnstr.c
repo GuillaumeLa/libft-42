ac2a072e095fd8f55469d5e93e3965a9d2fe2fd1
Guillaume Labadens D'Arc <glabaden@paul-f3Ar8s1.clusters.42paris.fr>
2023-11-07 12:14:18 +0100

    AUTOSAVE

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -9,5 +9,27 @@
 /*   Updated: 2023/11/06 11:02:56 by glabaden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+char    *ft_strnstr(char *str, char *to_find, int n)
+{
+    int    i;
+    int    j;
 
+    i = 0;
+    if (!to_find[0])
+        return (str);
+    if (!str[0])
+        return (0);
+    while (str[i] && i != n)
+    {
+        j = 0;
+        while (str[i + j] == to_find[j] )
+        {
+            j++;
+            if (!to_find[j])
+                return (str + i);
+        }
+        i++;
+    }
+    return (0);
+}
 
